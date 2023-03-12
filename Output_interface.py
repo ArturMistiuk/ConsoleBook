a43ab7b1adf33b7ec31c4f96e216cc04b4fdb145
Artur Mistiuk <mistiuk.artur@gmail.com>
2023-03-12 21:21:55 +0100

    Update Output_interface.py

--- Output_interface.py
+++ Output_interface.py
@@ -1,2 +1,19 @@
-from MyChatGPT import AddressBook
+from main import get_contacts
+from abc import ABC, abstractmethod
 
+
+class OutputInterface(ABC):
+
+    @abstractmethod
+    def show_contacts(self, *args, **kwargs):
+        pass
+
+
+class ConsoleInterface(OutputInterface):
+
+    def show_contacts(self, *args, **kwargs):
+        return get_contacts()
+
+
+output = ConsoleInterface()
+print(output.show_contacts())
